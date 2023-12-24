6c47b7b1698e55445e0e71d1235a3be08cfe8795
Mateusz <szachmat2004@gmail.com>
2023-12-24 21:41:40 +0100

    Refactoring initial_interface.cpp, adding constants.

--- Interface/initial_interface.cpp
+++ Interface/initial_interface.cpp
@@ -1,4 +1,5 @@
 #include "../functions_definitions.h"
+#include "interface.h"
 #include <string.h>
 #include <iostream>
 
@@ -19,13 +20,14 @@ int initial_interface(const SDL_Surfaces& surfaces, const SDL_Elements& SDL_elem
 	while (choosing_level)
 	{
 		// tekst informacyjny / info text
-		DrawRectangle(screen, 4, 4, SCREEN_WIDTH - 8, 60, colors.czerwony, colors.niebieski);
+		DrawRectangle(screen, DESC_RECTANGLE_INIT_X_Y, DESC_RECTANGLE_INIT_X_Y, 
+			DESC_RECTANGLE_WIDTH, DESC_RECTANGLE_HEIGHT, colors.czerwony, colors.niebieski);
 		sprintf(desc_text, "Embark on a  thrilling retro adventure  with my C++-crafted DonkeyKong");
-		DrawString(screen, screen->w / 2 - strlen(desc_text) * 8 / 2, 10, desc_text, charset);
+		DrawString(screen, screen->w / 2 - strlen(desc_text) * 8 / 2, DESC_LINE_1, desc_text, charset);
 		sprintf(desc_text, "game. You'll dodge barrels, climb ladders, and find amazing treasures!");
-		DrawString(screen, screen->w / 2 - strlen(desc_text) * 8 / 2, 28, desc_text, charset);
+		DrawString(screen, screen->w / 2 - strlen(desc_text) * 8 / 2, DESC_LINE_2, desc_text, charset);
 		sprintf(desc_text, "Enter - pick a level, \030 - choose lower level, \031 - choose higher level");
-		DrawString(screen, screen->w / 2 - strlen(desc_text) * 8 / 2, 45, desc_text, charset);
+		DrawString(screen, screen->w / 2 - strlen(desc_text) * 8 / 2, DESC_LINE_3, desc_text, charset);
 
 		SDL_UpdateTexture(SDL_elements.scrtex, NULL, screen->pixels, screen->pitch);
 		//		SDL_RenderClear(renderer);
@@ -38,22 +40,16 @@ int initial_interface(const SDL_Surfaces& surfaces, const SDL_Elements& SDL_elem
 
 		// Draw rectangle pointing to appropriate level
 		level_rectangle(screen, level, colors);
-
-		// Print levels informations
-		sprintf(desc_text, "Level 1");
-		DrawString(screen, screen->w / 2 - strlen(desc_text) * 8 / 2, 165, desc_text, charset);
-		sprintf(desc_text, "Level 2");
-		DrawString(screen, screen->w / 2 - strlen(desc_text) * 8 / 2, 270, desc_text, charset);
-		sprintf(desc_text, "Level 3");
-		DrawString(screen, screen->w / 2 - strlen(desc_text) * 8 / 2, 400, desc_text, charset);
-
-		// Print icons for each level
-		DrawSurface(screen, *(surfaces.level_1_crown),
-			SCREEN_WIDTH / 2, 120);
-		DrawSurface(screen, *(surfaces.level_2_treasure),
-			SCREEN_WIDTH / 2, 230);
-		DrawSurface(screen, *(surfaces.level_3_diamond),
-			SCREEN_WIDTH / 2, 360);
+		
+		SDL_Surface* level_logos[3] = { *(surfaces.level_1_crown) , *(surfaces.level_2_treasure) , *(surfaces.level_3_diamond) };
+		char* level_desc[3] = { "Level 1", "Level 2", "Level 3" };
+		// Print icons for each level, print levels informations
+		for (size_t i = 0; i < AMOUNT_OF_LEVELS; i++)
+		{
+			sprintf(desc_text, level_desc[i]);
+			DrawString(screen, screen->w / 2 - strlen(desc_text) * 8 / 2, INITIAL_STRING_Y + i * OFFSET, desc_text, charset);
+			DrawSurface(screen, level_logos[i], SCREEN_WIDTH / 2, INITIAL_SURFACE_Y + i * OFFSET);
+		}
 	}
     return 1;
 }
@@ -89,13 +85,13 @@ void level_rectangle(SDL_Surface* screen, const int level, const Colors& colors)
 	switch (level)
 	{
 	case 1:
-		DrawRectangle(screen, SCREEN_WIDTH / 2 - 75, 70, 150, 110, colors.jasny_niebieski, colors.czarny);
+		DrawRectangle(screen, RECTANGLES_X, RECTANGLE_1_Y, RECTANGLE_WIDTH, RECTANGLE_HEIGHT, colors.jasny_niebieski, colors.czarny);
 		break;
 	case 2:
-		DrawRectangle(screen, SCREEN_WIDTH / 2 - 75, 190, 150, 110, colors.jasny_niebieski, colors.czarny);
+		DrawRectangle(screen, RECTANGLES_X, RECTANGLE_2_Y, RECTANGLE_WIDTH, RECTANGLE_HEIGHT, colors.jasny_niebieski, colors.czarny);
 		break;
 	case 3:
-		DrawRectangle(screen, SCREEN_WIDTH / 2 - 75, 310, 150, 110, colors.jasny_niebieski, colors.czarny);
+		DrawRectangle(screen, RECTANGLES_X, RECTANGLE_3_Y, RECTANGLE_WIDTH, RECTANGLE_HEIGHT, colors.jasny_niebieski, colors.czarny);
 		break;
 	default:
 		break;
--- Interface/interface.h
+++ Interface/interface.h
@@ -4,3 +4,22 @@
 	Initial interface constants
 */
 
+const int AMOUNT_OF_LEVELS = 3;
+
+const int RECTANGLE_HEIGHT = 110;
+const int RECTANGLE_WIDTH = 150;
+const int RECTANGLES_X = SCREEN_WIDTH / 2 - 75;
+const int RECTANGLE_1_Y = 70;
+const int RECTANGLE_2_Y = 190;
+const int RECTANGLE_3_Y = 310;
+
+const int OFFSET = 120;
+const int INITIAL_SURFACE_Y = 120;
+const int INITIAL_STRING_Y = 165;
+
+const int DESC_RECTANGLE_INIT_X_Y = 4;
+const int DESC_RECTANGLE_WIDTH = SCREEN_WIDTH - 8;
+const int DESC_RECTANGLE_HEIGHT = 60;
+const int DESC_LINE_1 = 10;
+const int DESC_LINE_2 = 28;
+const int DESC_LINE_3 = 45;
