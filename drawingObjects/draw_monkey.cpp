3fda44d81d70591c32a6105096288e8ecd6f3cc5
Mateusz <szachmat2004@gmail.com>
2023-12-31 19:23:06 +0100

    Fixing bug with displaying monkey.

--- drawingObjects/draw_monkey.cpp
+++ drawingObjects/draw_monkey.cpp
@@ -3,15 +3,9 @@
 
 void DrawMonkey(const SDL_Surfaces& surfaces, const BoardElements& board, const TimeVariables& time)
 {
-	double dif = 0;
+	double dif = time.worldTime;
 	if (time.worldTime != 0 && (int(time.worldTime + 1) % FALLING_BARELL_TIME == 0) && time.worldTime > 1)
-	{
-		while (dif < FALLING_BARELL_TIME)
-		{
-			DrawSurface(*(surfaces.screen), *(surfaces.king_kong_throwing_barell), board.king_kong_x, board.king_kong_y);
-			dif += 0.003;
-		}
-	}
+		DrawSurface(*(surfaces.screen), *(surfaces.king_kong_throwing_barell), board.king_kong_x, board.king_kong_y);
 	else
 		DrawSurface(*(surfaces.screen), *(surfaces.king_kong), board.king_kong_x, board.king_kong_y);
 }
